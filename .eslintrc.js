103ed33b4fb32b465ff33b959dcc9158291eb83c
GlacJAY <glacjay@gmail.com>
2021-11-23 16:22:15 +0800

    style: auto sort imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,17 @@
 module.exports = {
   root: true,
   extends: 'react-app',
+  plugins: ['import', 'simple-import-sort'],
   rules: {
+    'import/first': 0,
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
     'no-console': 0,
     'no-unused-vars': 1,
     'react/prop-types': 0,
     'react/display-name': 0,
     'react-native/no-inline-styles': 0,
+    'simple-import-sort/imports': 'error',
+    'sort-imports': 'off',
   },
 };
