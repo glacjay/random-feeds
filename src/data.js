060b893e322b034774b12cbcb27e1b0c06608025
Keyboarder Jack <glacjay@gmail.com>
2022-01-03 18:13:50 +0800

    refactor: react-query: RandomItems

--- src/data.js
+++ src/data.js
@@ -1,5 +1,9 @@
 import { useQuery } from 'react-query';
 
+import api2 from './utils/api2';
+
+const LOADING_COUNT = 7;
+
 export function useToken() {
   const query = useQuery('token', () => localStorage.getItem('token'));
   return { ...query, token: query.data };
@@ -14,15 +18,101 @@ export function useFolders() {
   return { ...result, folders: result.data };
 }
 
-export function useFolderUnreadsCount(folder) {
+function useAllUnreadCounts() {
   const { token } = useToken();
   const result = useQuery('/reader/api/0/unread-count?output=json', {
     enabled: !!token,
-    select: (data) => {
-      const folderId = folder.id?.replace(/\d+/, '-');
-      const count = data.unreadcounts.find((c) => c.id === folderId);
-      if (count) return count.count;
-    },
+    select: (data) => data.unreadcounts,
   });
-  return { ...result, unreadsCount: result.data };
+  return { ...result, unreadCounts: result.data };
+}
+
+export function useFolderUnreadsCount(folder) {
+  const folderId = folder.id?.replace(/\d+/, '-');
+  const result = useAllUnreadCounts();
+  return { ...result, unreadsCount: result.unreadCounts?.find((uc) => uc.id === folderId)?.count };
+}
+
+function useAllSubscriptions() {
+  const { token } = useToken();
+  const result = useQuery('/reader/api/0/subscription/list?output=json', {
+    enabled: !!token,
+    select: (data) => data.subscriptions,
+  });
+  return { ...result, allSubscriptions: result.data };
+}
+
+function useFolderSubscriptions(folderId) {
+  const { unreadCounts } = useAllUnreadCounts();
+  const { allSubscriptions } = useAllSubscriptions();
+  return allSubscriptions?.filter(
+    (sub) =>
+      sub.categories?.some((cat) => cat.id === folderId) &&
+      unreadCounts?.find((uc) => uc.id === sub.id)?.count > 0,
+  );
+}
+
+export function useRandomItems({ folderId, isReloading }) {
+  const subscriptions = useFolderSubscriptions(folderId);
+  const result = useQuery(
+    ['randomItems', folderId],
+    async () => {
+      const localRandomItems = JSON.parse(localStorage.getItem(`randomItems:${folderId}`) || '[]');
+      if (localRandomItems.length > Math.random() * LOADING_COUNT) return localRandomItems;
+
+      for (let i = subscriptions.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [subscriptions[i], subscriptions[j]] = [subscriptions[j], subscriptions[i]];
+      }
+
+      let newItemsArray = await Promise.all(
+        subscriptions.slice(0, LOADING_COUNT).map(
+          async (subscription) =>
+            (
+              await api2.get('/reader/api/0/stream/items/ids', {
+                output: 'json',
+                s: subscription.id,
+                xt: 'user/-/state/com.google/read',
+                r: 'o',
+                n: LOADING_COUNT,
+              })
+            ).itemRefs,
+        ),
+      );
+
+      const loadingItems = [];
+      let addedCount = 0;
+      while (newItemsArray.length > 0) {
+        const newItems = newItemsArray.shift();
+        if (newItems.length > 0) {
+          const newItem = newItems.shift();
+          if (!loadingItems.some((item) => item.id === newItem.id)) {
+            loadingItems.push(newItem);
+            addedCount += 1;
+            if (addedCount >= 2 * LOADING_COUNT) break;
+          }
+          newItemsArray.push(newItems);
+        }
+      }
+
+      const newRandomItems = await Promise.all(
+        loadingItems.map(async (item) => ({
+          ...(
+            await api2.get(`/reader/api/0/stream/items/contents?output=json&i=${item.id}`)
+          ).items[0],
+          id: item.id,
+        })),
+      );
+
+      const randomItems = [
+        ...(isReloading ? [] : localRandomItems || []),
+        ...newRandomItems,
+      ].filter((item, pos, self) => self.findIndex((i2) => i2.id === item.id) === pos);
+
+      localStorage.setItem(`randomItems:${folderId}`, JSON.stringify(randomItems));
+      return randomItems;
+    },
+    { enabled: subscriptions?.length > 0 },
+  );
+  return { ...result, randomItems: result.data };
 }
--- src/pages/FolderPage.js
+++ src/pages/FolderPage.js
@@ -3,7 +3,7 @@ import { observer } from 'mobx-react';
 import qs from 'qs';
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import { useFolders, useFolderUnreadsCount } from 'src/data';
+import { useFolders, useFolderUnreadsCount, useRandomItems } from 'src/data';
 import { useRootStore } from 'src/RootStore';
 import { useToast } from 'src/utils/useToast';
 import ItemActions from 'src/widgets/ItemActions';
@@ -31,68 +31,17 @@ export default observer(function FolderPage(props) {
   return (
     <div className="flex-column" style={{ paddingBottom: 4 }}>
       <div style={{ margin: '4px 4px 0' }}>
-        {folder?.id?.replace(/.*\//g, '')}：{folder?.randomItems?.length}/
+        {folder?.id?.replace(/.*\//g, '')}：<RandomItemsCount folder={folder} />/
         <FolderUnreadsCount folder={folder} />
       </div>
 
-      {folder?.randomItems?.map((item) => (
-        <div
-          key={item.id}
-          style={{
-            margin: 4,
-            marginBottom: 0,
-            border: '1px solid black',
-            borderRadius: 4,
-            padding: 8,
-          }}
-        >
-          <Link to={`/Item?${qs.stringify({ folderId, id: item.id })}`}>
-            <div>{item.title}</div>
-            <div
-              className="flex-row align-center"
-              style={{ marginTop: 8, justifyContent: 'space-between', fontSize: 12, color: 'gray' }}
-            >
-              <div>
-                {item.origin?.title} | {item.author}
-              </div>
-              <div>{dayjs(item.updated * 1000).format('YYYY-MM-DD HH:mm')}</div>
-            </div>
-          </Link>
-          <div
-            className="flex-row align-center"
-            style={{ marginTop: 8, justifyContent: 'flex-end' }}
-          >
-            <ItemActions
-              folderId={folderId}
-              item={item}
-              isSubmitting={isSubmitting}
-              setIsSubmitting={setIsSubmitting}
-            />
-          </div>
-        </div>
-      ))}
+      <RandomItems folder={folder} />
 
       <div style={{ height: 100 }} />
       <div
         className="flex-row"
         style={{ position: 'fixed', bottom: 0, left: 0, right: 0, zIndex: 7, height: 50 }}
       >
-        {Math.random() > 2 && (
-          <button
-            onClick={async () => {
-              setIsSubmitting(true);
-              await rootStore.markItemsAsRead(folder.randomItems.map((item) => item.id));
-              await rootStore.loadItems({ folderId });
-              setIsSubmitting(false);
-            }}
-            disabled={isSubmitting}
-            className="button"
-            style={{ height: 44, opacity: isSubmitting ? 0.5 : 1, flex: 1 }}
-          >
-            mark them as read
-          </button>
-        )}
-
         <button
           onClick={() => {
             setIsSubmitting(true);
@@ -115,4 +64,53 @@ function FolderUnreadsCount({ folder }) {
   useToast(error);
 
   return <Fragment>{unreadsCount}</Fragment>;
+}
+
+function RandomItemsCount({ folder }) {
+  const { randomItems, error } = useRandomItems({ folderId: folder.id });
+  useToast(error);
+
+  return <Fragment>{randomItems?.length}</Fragment>;
+}
+
+function RandomItems({ folder }) {
+  const { randomItems, error } = useRandomItems({ folderId: folder.id });
+  useToast(error);
+
+  return (
+    <Fragment>
+      {randomItems?.map((item) => (
+        <div
+          key={item.id}
+          style={{
+            margin: 4,
+            marginBottom: 0,
+            border: '1px solid black',
+            borderRadius: 4,
+            padding: 8,
+          }}
+        >
+          <Link to={`/Item?${qs.stringify({ folderId: folder.id, id: item.id })}`}>
+            <div>{item.title}</div>
+            <div
+              className="flex-row align-center"
+              style={{ marginTop: 8, justifyContent: 'space-between', fontSize: 12, color: 'gray' }}
+            >
+              <div>
+                {item.origin?.title} | {item.author}
+              </div>
+              <div>{dayjs(item.updated * 1000).format('YYYY-MM-DD HH:mm')}</div>
+            </div>
+          </Link>
+
+          <div
+            className="flex-row align-center"
+            style={{ marginTop: 8, justifyContent: 'flex-end' }}
+          >
+            <ItemActions folderId={folder.id} item={item} />
+          </div>
+        </div>
+      ))}
+    </Fragment>
+  );
 }
