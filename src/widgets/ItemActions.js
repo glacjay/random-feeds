079fd6e9dbabfa21f040979b1573328ea5cf74d0
Keyboarder Jack <glacjay@gmail.com>
2025-10-09 18:29:55 +0800

    feat: button to mark & open item

--- src/widgets/ItemActions.js
+++ src/widgets/ItemActions.js
@@ -6,32 +6,46 @@ import { toast } from 'react-toastify';
 
 import { markAsRead, removeItem } from '@/app/api/actions';
 
-export async function ItemActions({ folderId, item, buttonStyle }) {
+export function ItemActions({ folderId, item, buttonStyle }) {
   let router = useRouter();
   let [isSubmitting, setIsSubmitting] = useState(false);
 
   if (!item?.id) return null;
 
+  const executeAndMarkAsRead = async (action) => {
+    try {
+      setIsSubmitting(true);
+      action?.();
+      await markAsRead(folderId, item.id);
+      router.back();
+    } catch (ex) {
+      console.warn('ItemActions.markAsRead error:', ex);
+      toast.error(ex.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
     <Fragment>
       <button
-        onClick={async () => {
-          try {
-            setIsSubmitting(true);
-            await markAsRead(folderId, item.id);
-            router.back();
-          } catch (ex) {
-            console.warn('ItemActions.markAsRead error:', ex);
-            toast.error(ex.message);
-          } finally {
-            setIsSubmitting(false);
-          }
-        }}
+        onClick={() => executeAndMarkAsRead()}
         disabled={isSubmitting}
         className="button"
         style={{ opacity: isSubmitting ? 0.5 : 1, ...buttonStyle }}
       >
         mark as read
+      </button>
+
+      <button
+        onClick={() =>
+          executeAndMarkAsRead(() => window.open(item?.canonical?.[0]?.href, '_blank'))
+        }
+        disabled={isSubmitting}
+        className="button"
+        style={{ opacity: isSubmitting ? 0.5 : 1, ...buttonStyle }}
+      >
+        read & open
       </button>
 
       <a
